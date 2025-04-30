e89a5c267b68eed995b483c32b136c92229e33e8
supportjeadatasub <jeadatasub@gmail.com>
2025-04-30 16:53:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,5 +18,8 @@ app.register_blueprint(auth_bp, url_prefix='/api')
 app.register_blueprint(wallet_bp, url_prefix='/api')
 app.register_blueprint(general_bp, url_prefix='/api')
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
