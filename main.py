1dd2250d9fa83d10dd3e988d3919899c72fd7e78
supportjeadatasub <jeasubdata@gmail.com>
2025-05-09 00:38:56 +0100

    Enable CORS to allow frontend signup

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask_cors import CORS
 from models.user import db
 from routes.auth import auth_bp
 from routes.wallet import wallet_bp
 from routes.general import general_bp
 
 app = Flask(__name__)
+# Allow any site (including your Netlify domain) to access /api/*
+CORS(app, resources={r"/api/*": {"origins": "*"}})
+
 app.config['SECRET_KEY'] = 'dev_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -19,7 +23,6 @@ app.register_blueprint(wallet_bp, url_prefix='/api')
 app.register_blueprint(general_bp, url_prefix='/api')
 
 import os
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
